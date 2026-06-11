212e4ce84a545aae7c3975a19bcf4f5876a8fccd
Ingenium-LiDAR <ingenium.lidar@outlook.com>
2026-06-11 08:08:59 -0500

    Added working code to install lidarslam_ros2

--- agent_scripts/Install_SLAM.sh
+++ agent_scripts/Install_SLAM.sh
@@ -18,11 +18,23 @@ echo -e "\e[38;5;5m If you got depreciation warnings and such, but nothing label
 source ~/Apps/ros2_ws/src/ndt_omp_ros2/install/setup.bash
 
 #FK test it!
-ros2 run ndt_omp_ros2 align data/251370668.pcd data/251371071.pcd
+# ros2 run ndt_omp_ros2 align data/251370668.pcd data/251371071.pcd
 
 # INSTALL THE MAIN SLAM GITHUB
 # insert stuff here
 
+#AB Install the slam repo and some more of its dependencies
+sudo apt install python3-rosdep -y #AB install rosdep, which I guess doens't come  by default! NB! python3-rosdep2 is only for Debian--python3-rosdep is for Ubuntu 
+sudo rosdep init #AB turn on rosdep
+rosdep update  #AB update rosdep
+
+cd ~/Apps/ros2_ws/src
+git clone --recursive https://github.com/rsasaki0109/lidarslam_ros2
+touch ~/Apps/ros2_ws/src/lidarslam_ros2/Thirdparty/ndt_omp_ros2/COLCON_IGNORE #AB Tell colcon to ignore the ndt_amp_ros2 package which comes bundled with the git repo
+cd ..
+rosdep install --from-paths src --ignore-src -r -y #AB Automatically install dependencies of the SLAM repo
+
+colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release #AB Colcon build the SLAM package
 
 echo 'source ~/Apps/ros2_ws/src/ndt_omp_ros2/install/setup.bash' >> ~/.bashrc #AB Add command to source the SLAM package upon terminal startup to the system ~/.bashrc file.
 echo -e "\e[38;5;82mSLAM installation complete.\033[0m"
